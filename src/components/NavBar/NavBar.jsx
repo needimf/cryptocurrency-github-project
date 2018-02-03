b49f23213db5e8f3c52f042d647d541fcc02bf27
Nedim Filipovic <nedim.filipovic@gmail.com>
2018-02-03 15:22:25 -0800

    Implemented navbar menu toggle on mobile.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,10 +4,15 @@ class NavBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      navbarMenuStatus: ''
     };
   }
   /*---------- Callback Methods ----------*/
+  handleNavBarHamburgerClick = () => {
+    this.setState(prevState => ({
+      navbarMenuStatus: prevState.navbarMenuStatus ? '' : ' is-active'
+    }))
+  }
 
   /*---------- Lifecycle Methods ----------*/ 
   render() {
@@ -16,13 +21,16 @@ class NavBar extends Component {
         <div className='navbar-brand'>
           <p className='navbar-item is-size-4 has-text-weight-semibold'>Crypto Github Projects</p>
 
-          <button className='button navbar-burger is-dark'>
+          <button 
+            className={`button navbar-burger is-dark${this.state.navbarMenuStatus}`}
+            onClick={() => this.handleNavBarHamburgerClick()}
+          >
             <span></span>
             <span></span>
             <span></span>
           </button>
         </div>
-        <div className='navbar-menu'>
+        <div className={`navbar-menu${this.state.navbarMenuStatus}`}>
           <div className='navbar-end'></div>
             <div className='navbar-item'>
               <button className='button is-black'>Top 6</button>

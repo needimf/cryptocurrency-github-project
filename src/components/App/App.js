2ef8febbdabec047c3521d3bac1e4c7cfb5bdf14
Nedim Filipovic <nedim.filipovic@gmail.com>
2018-02-03 15:36:54 -0800

    Stubbed up main page component structure.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './../NavBar/NavBar';
+import Cards from './../Cards/Cards';
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <section className='section'>
+          <div className='container'>
+            <h1 className='title is-3 has-text-centered'>Top 6 Cryptocurrency/Blockchain Projects</h1>
+            <h2 className='subtitle is-5 has-text-centered'>By Total Market Capitilization</h2>
+          </div>
+        </section>
+        <section className='section'>
+          <Cards />
+        </section>
       </div>
     );
   }
